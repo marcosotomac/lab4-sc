f9b3777e18e42c8f45d173a6d88e525dd27dfee7
Marco Soto Maceda <158858927+marcosotomac@users.noreply.github.com>
2025-11-07 15:19:09 -0500

    fix

--- pregunta1.c
+++ pregunta1.c
@@ -1,25 +1,12 @@
-/*
-.section .text
-.globl printone
+#include <stdio.h>
 
-printone:
-addi a7, x0, 64      # syscall: write (RISC-V)
-addi a0, x0, 1       # fd = 1 (stdout)
-mv   a1, t0          # buffer = *(t0) (puntero a 1 char)
-addi a2, x0, 1       # count = 1
-ecall
-ret                  # volver a C
-`*/
-
-// Declaramos la función ensamblada (no recibe args; usa t0 como canal)
 extern void printone(void);
 
 static void print_str(const char *s) {
     for (; *s; ++s) {
-        // Colocamos el puntero al char actual en el registro t0
-        register const char *p asm("t0") = s;
-        (void)p;  // evita warning "unused variable"
-        printone();  // imprime exactamente 1 carácter
+        // Colocar el puntero del carácter actual en t0
+        asm volatile("mv t0, %0" :: "r"(s));
+        printone();  // imprime un carácter
     }
 }
 
@@ -27,3 +14,5 @@ int main(void) {
     print_str("Hola RISC-V!\n");
     return 0;
 }
+
+//riscv32-unknown-linux-gnu-gcc -o programa32 pregunta1.c printone.s qemu-riscv32 ./programa32
